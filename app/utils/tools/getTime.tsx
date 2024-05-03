aa12149f84a2c677f92f7938813928b020131c3e
Developers Digest <developers.digest.ai@gmail.com>
2024-05-03 19:54:46 -0400

    hold

--- app/utils/tools/getTime.tsx
+++
@@ -1,5 +0,0 @@
-import { traceable } from "langsmith/traceable";
-export const getTime = traceable(async () => {
-
-    return 'The time is now being displayed on your palm.';
-}, { name: 'getTime' });

83c36c1988d5c9701e5065d5ae797f30ebe6dedd
Developers Digest <developers.digest.ai@gmail.com>
2024-05-06 22:51:44 -0400

    clean up layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AI } from "./action";
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
